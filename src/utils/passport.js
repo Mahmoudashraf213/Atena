c78021dbebe351cdf3ee0019a3acfe93c690d49f
Mahmoudashraf213 <mhmwdbhjt307@gmail.com>
2025-10-15 22:43:37 +0300

    fix: Correct email assignment in user creation within Google authentication

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -21,7 +21,7 @@ passport.use(
           // Create a new user if not found
           const newUser = new User({
             name: profile.displayName,
-            email,
+            email:emails[0].value,
             password: null,
             status: "verified", // from your enum
             otpVerified: true,
